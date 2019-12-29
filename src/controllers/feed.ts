c59a840ea52b4a87683cbe19179fa70e9a7fce57
vietanhcao <vietanhcao1994@gmail.com>
2019-12-29 17:10:16 +0700

    add express-validator

--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -1,12 +1,44 @@
 import { RequestHandler } from 'express';
+import { validationResult } from 'express-validator';
+
 
 export const getPosts: RequestHandler = (req, res, next) => {
-	res.status(200).json({ posts: [ { title: 'First Post', content: 'This is the first post!' } ] });
+	res.status(200).json({
+		posts: [
+			{
+				_id: '1',
+				title: 'First Post',
+				content: 'This is the first post!',
+				imageUrl: 'images/16174449_1120050004771102_6238590716381029991_n.jpg',
+				creator: {
+					name: 'viet anh'
+				},
+				createdAt: new Date()
+			}
+		]
+	});
 };
 export const createPost: RequestHandler = (req, res, next) => {
+	const errors = validationResult(req);
+
+	if(!errors.isEmpty()){
+		res.status(422).json({
+			message: 'validation failed, entered data is incorrect.',
+			errors: errors.array()
+		})
+	}
 	const { title, content } = req.body;
 	// Create post in db
-	res
-		.status(201)
-		.json({ message: 'Post created successfully!', post: { id: new Date().toISOString(), title, content } });
+	res.status(201).json({
+		message: 'Post created successfully!',
+		post: {
+			_id: new Date().toISOString(),
+			title,
+			content,
+			creator: {
+				name: 'viet anh'
+			},
+			createdAt: new Date()
+		}
+	});
 };
--- src/router/feed.ts
+++ src/router/feed.ts
@@ -1,15 +1,23 @@
-import express from "express";
+import express from 'express';
 import { getPosts, createPost } from '../controllers/feed';
-
+import { body } from 'express-validator';
 
 const router = express.Router();
 
-//get /feed/post
-router.get('/posts', getPosts)
+//get /feed/posts
+router.get('/posts', getPosts);
 //post /feed/post
-router.post('/post', createPost)
-
-
-
+router.post(
+	'/post',
+	[
+		body('title').trim().isLength({
+			min: 7
+		}),
+		body('content').trim().isLength({
+			min: 5
+		})
+	],
+	createPost
+);
 
 export default router;
