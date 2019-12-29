ce7b0ff13f7e384c06ce506b334b8c83066041ef
vietanhcao <vietanhcao1994@gmail.com>
2019-12-29 22:38:22 +0700

    add hanlding error and static file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
+import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import feedRoutes from './router/feed';
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const app = express();
 
 // app.use(bodyParser.urlencoded())// x-www-form-urlencoded use form
 
 app.use(bodyParser.json()); // application/json
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
 	//access CROS
@@ -18,8 +21,17 @@ app.use((req, res, next) => {
 });
 app.use('/feed', feedRoutes);
 
-mongoose.connect('mongodb+srv://vietanhcao1994:sao14111@cluster0-ardsb.mongodb.net/test?retryWrites=true&w=majority').then(result => {
-	app.listen(8080);
-}).catch(err => console.log(err))
+app.use((error, req, res, next) => {
+	console.log('TCL: error', error);
+	const { statusCode, message } = error;
+	res.status(statusCode || 500).json({
+		message
+	});
+});
 
-
+mongoose
+	.connect('mongodb+srv://vietanhcao1994:sao14111@cluster0-ardsb.mongodb.net/message?retryWrites=true&w=majority') //create db message
+	.then((result) => {
+		app.listen(8080);
+	})
+	.catch((err) => console.log(err));
--- src/controllers/feed.ts
+++ src/controllers/feed.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 import { validationResult } from 'express-validator';
 
+import Post from '../model/post';
 
 export const getPosts: RequestHandler = (req, res, next) => {
 	res.status(200).json({
@@ -18,27 +19,35 @@ export const getPosts: RequestHandler = (req, res, next) => {
 		]
 	});
 };
-export const createPost: RequestHandler = (req, res, next) => {
-	const errors = validationResult(req);
-
-	if(!errors.isEmpty()){
-		res.status(422).json({
-			message: 'validation failed, entered data is incorrect.',
-			errors: errors.array()
-		})
-	}
-	const { title, content } = req.body;
-	// Create post in db
-	res.status(201).json({
-		message: 'Post created successfully!',
-		post: {
-			_id: new Date().toISOString(),
+export const createPost: RequestHandler = async (req, res, next) => {
+	try {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			let error = new Error('Validation failed, entered data is incorrect.');
+			(error as any).statusCode = 422;
+			throw error;
+		}
+		const { title, content } = req.body;
+		// Create post in db
+		const post = new Post({
 			title,
 			content,
+			imageUrl: 'images/16174449_1120050004771102_6238590716381029991_n.jpg',
 			creator: {
 				name: 'viet anh'
-			},
-			createdAt: new Date()
+			}
+		});
+		const result = await post.save();
+
+		res.status(201).json({
+			message: 'Post created successfully!',
+			post: result
+		});
+	} catch (error) {
+		if (!error.statusCode) {
+			error.statusCode = 500;
 		}
-	});
+		next(error);
+		
+	}
 };
